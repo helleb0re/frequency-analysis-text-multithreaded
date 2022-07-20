223ab4b2dcaf8036229dcebad381e11e81abc998
helleb0re <34608677+helleb0re@users.noreply.github.com>
2022-07-20 20:05:00 +0300

    Added comments and fixed some variables

--- text_frequency_analysis/Program.cs
+++ text_frequency_analysis/Program.cs
@@ -1,102 +1,68 @@
 ﻿using System.Diagnostics;
 using text_frequency_analysis;
 
-
+// Инициализация и включение таймера
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
-int syllableLength = 3;
-string s = File.ReadAllText("input.txt");
+// Чтение данных из файла, путь к которому указывается при запуске программы
+string s = File.ReadAllText(args[0]);
 
+int syllableLength = 3; // Длина набора символов слова
 int numThreads = Environment.ProcessorCount;
-int chunk = s.Length / numThreads;
-int d = s.Length % numThreads;
+int chunk = s.Length / numThreads; // Длина текста, обработка которого будет выполняться одним потоком
+int d = s.Length % numThreads; // Остаток текста, который необходимо определить какому-либо потоку дополнительно
 
 Thread[] threads = new Thread[numThreads];
 
-Dictionary<string, int> strCounts = new Dictionary<string, int>();
+Dictionary<string, int> strCounts = new Dictionary<string, int>(); // Общий сбор данных
+// Цикл для запуска потоков
 for (int i = 0; i < numThreads; i++)
 {
+    // Получаем срез строки
     string shortStr = s.Substring(i * chunk, chunk + (i + 1 == numThreads ? d : 0));
-    
+    // Учитываем тот факт, что задача разбивается на несколько потоков, поэтому целостность текста нарушается
+    // Для этого нужно добавить к срезу дополнительно последние символы из предыдущего среза
     if (i != 0) shortStr = s.Substring(i * chunk - syllableLength + 1, syllableLength - 1) + shortStr;
-
+    
+    // Подготавливаем поток на запуск
     threads[i] = new Thread(() =>
     {
-        // // var tasks = new List<Task<Dictionary<string, int>>>();
-        // int k = 10;
-        // var tasks = new Task<Dictionary<string, int>>[k];
-        //
-        // int chunkTask = shortStr.Length / k;
-        // int dTask = shortStr.Length % k;
-        //
-        // for (int j = 0; j < k; j++)
-        // {
-        //     int index = j;
-        //     tasks[j] = new Task<Dictionary<string, int>>(() => 
-        //         TextAnalysis.FrequencySyllable(
-        //             shortStr.Substring(index * chunkTask, chunkTask + (index + 1 == k ? dTask : 0)),
-        //             3));
-        //     tasks[j].Start();
-        //     // tasks.Add(Task<Dictionary<string, int>>.Factory.StartNew(() => 
-        //     //     TextAnalysis.FrequencySyllable(sArray[index], 3)));
-        // }
-        //
-        // Task.WaitAll(tasks);
-        //
-        // var strCountsTasks = new Dictionary<string, int>();
-        //
-        // foreach (var t in tasks)
-        //     TextAnalysis.MergeDictionaries(ref strCountsTasks, t.Result);
-        
+        // запускаем метод, который непосредственно выполняет частотный анализ
         var dict = TextAnalysis.FrequencySyllable(shortStr, 3);
+        // блокируем общий сбор данных для того, чтобы добавить туда новые данные
         lock (strCounts)
         {
-            MergeDictionaries(ref strCounts, dict);
+            MergeDictionaries(strCounts, dict);
         }
         
     });
+    // запускаем поток
     threads[i].Start();
 }
 
+// ожидаем выполнение каждого потока
 foreach (Thread thread in threads)
-{
     thread.Join();
-}
 
-// var res = TextAnalysis.FrequencySyllable(s, 3);
-
-// Console.WriteLine(res.Values.Sum());
-// Console.WriteLine(strCounts.Values.Sum());
-
-var res = strCounts;
-
-Console.WriteLine(String.Join(", ", res
+// сортируем исходные данные по значению и берем 10 первых
+var res = String.Join(", ", strCounts
     .OrderByDescending((x => x.Value))
     .Take(10)
-    .Select(x => $"{x.Key}={x.Value}")));
-
+    .Select(x => $"{x.Key}"));
+// останавливаем таймер
 stopWatch.Stop();
+// выводим результат
+Console.WriteLine(res);
 Console.WriteLine($"Затраченное время: {stopWatch.Elapsed.TotalMilliseconds}");
 
-
-static void MergeDictionaries(ref Dictionary<string, int> dict1, Dictionary<string, int> dict2)
+// функция для слияния общих данных и данных, полученных из второстепенных потоков
+static void MergeDictionaries(Dictionary<string, int> mainDict, Dictionary<string, int> dict)
 {
-    if (dict1.Count == 0)
+    foreach (KeyValuePair<string, int> kv in dict)
     {
-        dict1 = dict2.ToDictionary(entry => entry.Key, entry => entry.Value);
-    }
-    else
-    {
-        foreach (KeyValuePair<string,int> kv in dict2)
-        {
-            if (dict1.ContainsKey(kv.Key))
-            {
-                dict1[kv.Key] += kv.Value;
-            }
-            else
-            {
-                dict1[kv.Key] = kv.Value;
-            }
-        }
+        if (mainDict.ContainsKey(kv.Key))
+            mainDict[kv.Key] += kv.Value;
+        else
+            mainDict[kv.Key] = kv.Value;
     }
 }
--- text_frequency_analysis/TextAnalysis.cs
+++ text_frequency_analysis/TextAnalysis.cs
@@ -2,34 +2,39 @@
 
 public static class TextAnalysis
 {
+    // набор символов, которые не могут являться частью слова
     static readonly HashSet<char> PunctuationMarks = new() 
         {' ', '.', ',', '-', ';', ':', '[', '(', ']', ')', '"', '\'', '{', '}', '/', 
-            '\\', '\n', '\r', '\t', '!', '?'};
+            '\\', '\n', '\r', '\t', '!', '?', '<', '>', '+', '_', '=', '\f', '\v', '\0'};
 
     public static Dictionary<string, int> FrequencySyllable(string s, int syllableLength)
     {
+        // объявляем хранилище данных
         Dictionary<string, int> strCounts = new Dictionary<string, int>();
         
-        Queue<char> strQueue = new Queue<char>();
-
+        string syllable = "";
+        
+        // главный цикл
         foreach (char c in s)
         {
+            // проверяем, является ли символ недопустимым
             if (PunctuationMarks.Contains(c))
             {
-                strQueue.Clear();
+                // если да, то очищаем слог и переходим к следующему символу
+                syllable = "";
                 continue;
             }
-
-            strQueue.Enqueue(Char.ToLower(c));
-    
-            if (strQueue.Count < syllableLength) continue;
-
-            string str = new string(strQueue.ToArray());
-
-            if (strCounts.ContainsKey(str)) strCounts[str]++;
-            else strCounts[str] = 1;
-
-            strQueue.Dequeue();
+            // добавляем символ в проверяемый слог
+            syllable += Char.ToLower(c);
+            // если длина слога меньше заданного, то переходим к следующему символу
+            if (syllable.Length < syllableLength) continue;
+            
+            // заносим данные о найденном слоге
+            if (strCounts.ContainsKey(syllable)) strCounts[syllable]++;
+            else strCounts[syllable] = 1;
+            
+            // удаляем первый символ
+            syllable = syllable.Remove(0, 1);
         }
 
         return strCounts;
